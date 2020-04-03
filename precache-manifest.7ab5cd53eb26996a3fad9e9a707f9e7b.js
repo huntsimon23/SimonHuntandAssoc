1cbe60b574930ab99cee732a7caad3101ea919fe
huntsimon23 <hunt.simon23@gmail.com>
2020-04-03 14:44:47 -0400

    Updates

---
+++ precache-manifest.7ab5cd53eb26996a3fad9e9a707f9e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383dabd2dcea2426ce5f583e5d109068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b97036c6a1a5b815b40",
+    "url": "/static/css/main.3c2eb224.chunk.css"
+  },
+  {
+    "revision": "4244cbc6a65f46291dfd",
+    "url": "/static/js/2.87953436.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87953436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b97036c6a1a5b815b40",
+    "url": "/static/js/main.3910a612.chunk.js"
+  },
+  {
+    "revision": "560ac8bb0d6024c43449",
+    "url": "/static/js/runtime-main.0b1ef7f0.js"
+  }
+]);
